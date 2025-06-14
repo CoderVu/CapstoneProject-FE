31f8ffb69fda96e10937b47086b67e57f7d0d971
CoderVu <vunguyen.170803@gmail.com>
2025-06-15 00:29:08 +0700

    update https

--- src/redux/setup/axios.js
+++ src/redux/setup/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     // baseURL: 'https://capstoneproject-be-iapt.onrender.com',
    // baseURL: 'http://localhost:8080',
-    baseURL: 'http://20.3.131.196:8080',
+    baseURL: 'https://www.capstone.io.vn/api',
     withCredentials: true,
 });
 
--- src/redux/setup/ws.js
+++ src/redux/setup/ws.js
@@ -1,5 +1,5 @@
 // Định nghĩa URL WebSocket dùng chung cho toàn app
- export const WS_URL = "ws://20.3.131.196:8080/ws";
+ export const WS_URL = "wss://www.capstone.io.vn/api/ws";
 // export const WS_URL_ORDER = `${WS_URL}/orders`;   
 // export const WS_URL = "wss://capstoneproject-be-iapt.onrender.com/ws";
 export const WS_URL_ORDER = `${WS_URL}/orders`;
