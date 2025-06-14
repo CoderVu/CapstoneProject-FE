4426edc8d0aab1c8ccf2b1ec49871e3793e85c8d
CoderVu <vunguyen.170803@gmail.com>
2025-06-15 00:41:49 +0700

    update http->httpss

--- src/redux/setup/config/api.js
+++ src/redux/setup/config/api.js
@@ -1,7 +1,7 @@
 // API Configuration
 const API_CONFIG = {
  // BASE_URL: 'https://capstoneproject-be-iapt.onrender.com/api',
-  BASE_URL: 'http://20.3.131.196:5000/api',
+  BASE_URL: 'https://www.capstone.io.vn/api/api',
   
   // AI Endpoints
   AI: {
--- src/redux/setup/ws.js
+++ src/redux/setup/ws.js
@@ -1,5 +1,5 @@
 // Định nghĩa URL WebSocket dùng chung cho toàn app
- export const WS_URL = "wss://www.capstone.io.vn/api/ws";
+ export const WS_URL = "wss://www.capstone.io.vn/ws";
 // export const WS_URL_ORDER = `${WS_URL}/orders`;   
 // export const WS_URL = "wss://capstoneproject-be-iapt.onrender.com/ws";
 export const WS_URL_ORDER = `${WS_URL}/orders`;
