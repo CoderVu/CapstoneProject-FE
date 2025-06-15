73f0eb2ae3cad5cc75bc4af88b82e86160dbfd3a
CoderVu <vunguyen.170803@gmail.com>
2025-06-15 11:32:10 +0700

    fix wss

--- src/redux/setup/config/api.js
+++ src/redux/setup/config/api.js
@@ -1,7 +1,7 @@
 // API Configuration
 const API_CONFIG = {
   // BASE_URL: 'https://capstoneproject-be-iapt.onrender.com/api',
-  BASE_URL: 'https://www.capstone.io.vn/ai/api',
+  BASE_URL: 'https://www.capstone.io.vn/ai',
   // BASE_URL: 'http://127.0.0.1:5000',
 
   // AI Endpoints
