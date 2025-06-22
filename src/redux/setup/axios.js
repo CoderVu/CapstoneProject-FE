d0737cc1b52706f4a3cdbb838ad66ccab8382408
CoderVu <vunguyen.170803@gmail.com>
2025-06-22 14:07:39 +0700

    update code 22/6

--- src/redux/setup/axios.js
+++ src/redux/setup/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     // baseURL: 'https://capstoneproject-be-iapt.onrender.com',
     //  baseURL: 'http://localhost:8080',
-    baseURL: 'https://www.capstone.io.vn',
+    baseURL: 'https://www.capstone.io.vn/api',
     withCredentials: true,
 });
 
