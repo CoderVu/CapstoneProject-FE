c0618bc7ec0f4332a31c37884f7e940af0d08690
CoderVu <vunguyen.170803@gmail.com>
2025-06-22 13:53:10 +0700

    update code 22/6 and login gg

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fe/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -13,7 +14,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
+      '/': {
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
