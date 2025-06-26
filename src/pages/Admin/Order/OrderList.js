24eeef1b88ea742e6a891f9d7aac91be1c4277be
CoderVu <vunguyen.170803@gmail.com>
2025-06-26 10:34:20 +0700

    Commit: update totalSold on Shop final

--- src/pages/Admin/Order/OrderList.js
+++ src/pages/Admin/Order/OrderList.js
@@ -464,8 +464,8 @@ const OrderList = () => {
                               <p className="text-gray-700 text-sm"><span className="font-medium">Phương thức thanh toán:</span> {filteredOrders.find(order => order.orderCode === expandedOrderId).paymentMethod}</p>
                               <p className="text-gray-700 text-sm mt-1">
                                 <span className="font-medium">Trạng thái thanh toán:</span>{' '}
-                                <span className={filteredOrders.find(order => order.orderCode === expandedOrderId).status === 'PAID' ? "text-green-600" : "text-yellow-600"}>
-                                  {filteredOrders.find(order => order.orderCode === expandedOrderId).status === 'PAID' ? "Đã thanh toán" : "Chưa thanh toán"}
+                                <span className={filteredOrders.find(order => order.orderCode === expandedOrderId).status === 'PENDING' ? "text-yellow-600" : "text-green-600"}>
+                                  {filteredOrders.find(order => order.orderCode === expandedOrderId).status === 'PENDING' ? "Chưa thanh toán" : "Đã thanh toán"}
                                 </span>
                               </p>
                               <p className="text-gray-700 text-sm mt-1">
